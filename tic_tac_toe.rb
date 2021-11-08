98c8ae0a8dc3a7b0f249b2fabe45391dc9b18a08
patrick <patrick@o-neill.ca>
2021-11-07 21:47:49 -0500

    Refactored set up

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -1,18 +1,31 @@
-# frozen_string_literal: true
+class Person
+  attr_accessor :name
 
-class Board
-  attr_accessor :state
-
-  def initialize
-    self.state = [['_', '|', '_', '|', '_'],
-                  ['_', '|', '_', '|', '_'],
-                  [' ', '|', ' ', '|', ' ']]
-  end
-
-  def display_board
-    puts(state.map { |element| element.join(' ') })
+  def initialize(name)
+    @name = name
   end
 end
 
-new_board = Board.new
-new_board.display_board
+class Player < Person
+  attr_reader :marker
+
+  def initialize(name, marker)
+    super(name)
+    @marker = marker
+  end
+end
+
+class Board
+  WIN_COMBINATIONS = [[0, 1, 2],[3, 4, 5],[6, 7, 8],[0, 3, 6],
+                   [1, 4, 7],[2, 5, 8],[6, 4, 2],[0, 4, 8]]
+  def initialize
+    @board = [' ', ' ', ' ',  ', ' ', ' ',  ', ' ', ' ']
+  end
+end
+
+def turn
+  print "Please pick a number between 1-9"
+  position = gets.chomp.to_i
+  position -= 1
+  @board[position] = player.sign
+end
