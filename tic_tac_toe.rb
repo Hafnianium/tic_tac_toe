6cc10e53ec16b1aea0232dd29e1b44d26e2289bf
patrick <patrick@o-neill.ca>
2021-11-07 20:47:28 -0500

    Setup nested array for board

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -1,32 +1,18 @@
+# frozen_string_literal: true
+
 class Board
-  @@inital_state = [["_", "|", "_", "|", "_"], 
-  ["_", "|", "_", "|", "_"],
-  [" ", "|", " ", "|", " "]]
+  attr_accessor :state
+
   def initialize
-    @state = @@inital_state
+    self.state = [['_', '|', '_', '|', '_'],
+                  ['_', '|', '_', '|', '_'],
+                  [' ', '|', ' ', '|', ' ']]
   end
 
   def display_board
-    i = 0
-    while i < @state[0].length
-      print @state[0][i]
-      i += 1
-    end
-    print "\n"
-    i = 0
-    while i < @state[1].length
-      print @state[1][i]
-      i += 1
-    end
-    print "\n"
-    i = 0
-    while i < @state[2].length
-      print @state[2][i]
-      i += 1
-    end
+    puts(state.map { |element| element.join(' ') })
   end
 end
 
 new_board = Board.new
-
 new_board.display_board
