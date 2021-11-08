cf9f7bb85ddf5e993268b6140f327ea515d1c699
patrick <patrick@o-neill.ca>
2021-11-07 22:17:04 -0500

    Updated board display and player classes

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -19,7 +19,19 @@ class Board
   WIN_COMBINATIONS = [[0, 1, 2],[3, 4, 5],[6, 7, 8],[0, 3, 6],
                    [1, 4, 7],[2, 5, 8],[6, 4, 2],[0, 4, 8]]
   def initialize
-    @board = [' ', ' ', ' ',  ', ' ', ' ',  ', ' ', ' ']
+    @board = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
+  end
+
+  def display_board
+    puts "   |   |   "
+    puts " #{@board[0]} | #{@board[1]} |  #{@board[2]} "
+    puts "___|___|___"
+    puts "   |   |   "
+    puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
+    puts "___|___|___"
+    puts "   |   |   "
+    puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
+    puts "   |   |   "
   end
 end
 
@@ -29,3 +41,8 @@ def turn
   position -= 1
   @board[position] = player.sign
 end
+
+new_board = Board.new
+new_board.display_board
+
+
