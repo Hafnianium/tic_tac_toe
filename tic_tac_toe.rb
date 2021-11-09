d40a896b044b80a91be6d977947a8da63ca07f56
patrick <patrick@o-neill.ca>
2021-11-09 12:33:34 -0500

    winning combinations added

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -1,12 +1,17 @@
 class Board
   attr_accessor :spaces
 
+  WINNING_COMBINATIONS = [
+                          [0, 1, 2], [3, 4, 5], [6, 7, 8],
+                          [0, 3, 6], [1, 4, 7], [2, 5, 8],
+                          [0, 4, 8], [2, 4, 6]]
+
   def initialize
     @spaces = Array.new(9, ' ')
   end
 
   def display_board
-    puts "_____________"
+    puts '_____________'
     i = 0
     while i < @spaces.length
       puts '|   |   |   |'
@@ -24,7 +29,7 @@ end
 class Player
   attr_accessor :name, :marker
 
-  def initialize(name)
+  def initialize(name, marker)
     @name = name
     @marker = marker
   end
@@ -39,57 +44,58 @@ class Game
     @board = board
 
     @current_turn = 1
-    @first_turn = ""
-    @winner = ""
+    @first_turn = ''
+    @winner = ''
 
     play_game
   end
 
   def play_game
-    player_name_input
     while true
       take_turn
-    end
+      check_win
   end
 
   def take_turn
     @board.display_board
-    puts "X Player pick a number from 1-9."
+    puts 'X Player pick a number from 1-9.'
     player_input = (gets.chomp.to_i - 1)
     @board.add_marker(player_input, x_player.marker)
     @board.display_board
-    puts "O Player pick a number from 1-9."
+    puts 'O Player pick a number from 1-9.'
     player_input = (gets.chomp.to_i - 1)
     @board.add_marker(player_input, o_player.marker)
   end
 
-  def player_name_input
-    puts "X player enter your name"
-    @x_player.name = gets.chomp.to_s
-    @x_player.marker = "X"
-    puts "O player enter your name"
-    @o_player.name = gets.chomp.to_s
-    @o_player.marker = "O"
+  def check_win
+    WINNING_COMBINATIONS.each do |triplet|
+      @winner = x_player if triplet.all? {|element| @board.spaces[element] == x_player.marker}
+      end
+    end
+    WINNING_COMBINATIONS.each do |triplet|
+      @winner = o_player if triplet.all? {|element| @board.spaces[element] == o_player.marker}
+      end
+    end
   end
-end
 
 class TicTacToeStarter
   def initialize
     welcome_screen
     board = Board.new
-    x_player, o_player = Player.new(@name1), Player.new(@name2)
+    x_player = Player.new(@name1, 'X')
+    o_player = Player.new(@name2, 'O')
     Game.new(x_player, o_player, board)
   end
 
   def welcome_screen
-    puts "|| Welcome to Tic Tac Toe! ||"
+    puts '|| Welcome to Tic Tac Toe! ||'
     puts "||-------------------------||\n\n\n"
     print "Enter X player's name: "
     @name1 = gets.chomp
-    puts " "
+    puts ' '
     print "Enter O player's name: "
     @name2 = gets.chomp
-    puts " "
+    puts ' '
   end
 end
 
